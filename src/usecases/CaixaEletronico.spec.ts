e91c35631fa693e7b080053fb741837b33b28153
calebe oliveira <calebeoliver3@gmail.com>
2021-08-28 16:37:51 -0300

    test: Ao sacar 80, Entregar 1 nota de R$50,00 1 nota de R$ 20,00 e 1 nota de R$ 10,00

--- src/usecases/CaixaEletronico.spec.ts
+++ src/usecases/CaixaEletronico.spec.ts
@@ -68,3 +68,9 @@ describe('Teste Caixa Eletronico Dojo', () => {
         expect(sut.sacar(30)).toEqual(`Entregar 1 nota de ${formatadorMoedaDuble.formatar(20)} e 1 nota de ${formatadorMoedaDuble.formatar(10)}.`)
     })
 })
+
+test('Ao sacar 80, Entregar 1 nota de R$50,00 1 nota de R$ 20,00 e 1 nota de R$ 10,00.',()=>{
+    const {sut, formatadorMoedaDuble} = makeSut()
+
+    expect(sut.sacar(80)).toEqual(`Entregar 1 nota de ${formatadorMoedaDuble.formatar(50)}, 1 nota de ${formatadorMoedaDuble.formatar(20)} e 1 nota de ${formatadorMoedaDuble.formatar(10)}.`)
+})
