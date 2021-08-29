9b1ba5ba11efcf8f9b9284963aa31f0f89a11c8f
calebe oliveira <calebeoliver3@gmail.com>
2021-08-28 21:06:20 -0300

    test: formatador de moeda

---
+++ src/utils/formatar-moeda.spec.ts
@@ -0,0 +1,9 @@
+import { FormatadorMoeda } from "./formatar-moeda"
+
+describe('formatador de moedas', () => {
+  test('deve retornar R$ 10,00 quando o valor 10 for fornecido', () => {
+    const valorTeste = 10
+
+    expect(FormatadorMoeda.formatar(valorTeste)).toEqual(`R$ ${valorTeste},00`)
+  })
+})
