ff79a44292935efc3494dec3d3806af3f05a5be3
calebe oliveira <calebeoliver3@gmail.com>
2021-08-28 21:30:14 -0300

    Feat: App

---
+++ src/usecases/App.ts
@@ -0,0 +1,26 @@
+import { FormatadorMoeda } from '../utils/formatar-moeda'
+import { CaixaEletronico } from "./CaixaEletronico"
+import { Teclado } from '../utils/teclado'
+import { Gaveta } from "./Gaveta"
+
+const gaveta = new Gaveta([
+  { valor: 100, quantidade: 100 },
+  { valor: 50, quantidade: 100 },
+  { valor: 20, quantidade: 100 },
+  { valor: 10, quantidade: 100 }
+])
+
+const caixaEletronico = new CaixaEletronico(gaveta, FormatadorMoeda);
+
+(async () => {
+  while (true) {
+    const valor = await Teclado.ler('Quanto você deseja sacar?')
+    try {
+      const message = caixaEletronico.sacar(Number.parseInt(valor))
+      console.info("\n" + message)
+      console.log(`(saldo no caixa: ${gaveta.recuperarTotalDisponivel()})\n`)
+    } catch ({ message }) {
+      console.error(message)
+    }
+  }
+})()
