c3da20539e78463d80c4bbd43ed8ded76e6a971f
calebe oliveira <calebeoliver3@gmail.com>
2021-08-28 21:05:31 -0300

    fix: exportar interface compartimento

--- src/usecases/protocolos.ts
+++ src/usecases/protocolos.ts
@@ -1,10 +1,11 @@
-interface Compartimento {
+export interface Compartimento {
   valor: number, quantidade: number
 }
 
 export interface IGaveta {
   compartimentos: Compartimento[]
   recuperarTotalDisponivel: () => number
+  removerNotas: (notas: Compartimento) => void
   recuperarQuantidadeDeNotasDisponiveis: () => { valor: number, quantidade: number }[]
 }
 
